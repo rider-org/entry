81371fb44573a554cd07ca87e57099383c3f2b2e
Tahmid Ahmed <tahmid@tahmid.io>
2025-02-19 19:20:31 -0500

    Added more repositories to cloneRepos.ts

--- cloneRepos.ts
+++ cloneRepos.ts
@@ -1,11 +1,14 @@
 import { exec } from "child_process";
 
-const REPOS: { name: string; dir: string }[] = [];
+const REPOS: { name: string; dir: string }[] = [
+  { name: "rider-org/mobile", dir: "mobile" },
+  { name: "rider-org/server", dir: "server" }
+];
 
 for (const repo of REPOS) {
   exec(
     `git clone https://github.com/${repo.name} ${repo.dir}`,
-    (err, stdout, stdin) => {
+    (_, stdout, stdin) => {
       console.log(stdout, stdin);
     }
   );
