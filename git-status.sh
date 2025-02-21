6826ce370ad1849fd2d4a307642cd08b51a8fb01
Tahmid Ahmed <tahmid@tahmid.io>
2025-02-20 21:52:40 -0500

    Updated git-status.sh to shorten the remote repository to exclude the github.com url.

--- git-status.sh
+++ git-status.sh
@@ -4,7 +4,10 @@ find . -name .git -type d -execdir sh -c '
     REMOTE_URL=$(git remote get-url origin 2>/dev/null || echo "No remote found")
     LOCAL_DIR=$(pwd)
     
-    echo "\033[34mRemote Repository:\033[0m \033[32m$REMOTE_URL\033[0m"
+   # Remove "https://github.com/" from REMOTE_URL if it exists
+    CLEAN_REMOTE_URL=$(echo "$REMOTE_URL" | sed "s|https://github.com/||")
+
+    echo "\033[34mRemote Repository:\033[0m \033[32m$CLEAN_REMOTE_URL\033[0m" 
     echo "\033[34mLocal Directory:\033[0m \033[32m$LOCAL_DIR\033[0m"
     git status
     printf "\n" 
